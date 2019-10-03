8a0e50efb6dc991d509190df1d43701bd5010eb1
beisenstat18 <56099532+beisenstat18@users.noreply.github.com>
2019-10-02 22:58:10 -0400

    Add files via upload

--- BrachaEisenstat_Project_1*/project_text.js
+++ BrachaEisenstat_Project_1*/project_text.js
@@ -1,10 +1,12 @@
 var x= 0;
+var y=0;
 var r= 255;
 var b=215;
 var g=0;
 var size=10;
-var positionX= 300
-var positionY=400
+var positionX= 300;
+var positionY=400;
+var layer=1;
 
 function setup() {
 	createCanvas(600,400);
@@ -12,14 +14,30 @@ function setup() {
 }
 
 function draw() {
-	x++;
-	positionY--;
-	size++;
+	
 	r=map(x,0,600,255,255);
-	b=map(x,0,600,96,215);
+	b=map(x,0,600,96,255);
 	background(r,b,0);
+	size++;
+	positionY--;
+	noStroke();
+	fill('#FEE2F1');
+	shape_1();
+	//y++;
+	//x++;
+	//shape_2();
+	//if (positionY==0){
+	//nofill();
+		//shape_2();
 	
+}
+
+function shape_1 (){
 	ellipse(positionX, positionY, size, size);
-	
+
+function shape_2(){
+	arc(x,y, 40, 40, PI+QUARTER_PI, PIE);
+
+}	
 	
 }
